ba0929a993b5245bc8bb364815a376546c1447c2
Lucy Sheehy <lucysheehy@yahoo.com>
2020-02-06 17:22:09 +0000

    Add features file and StepDefinitions class with first step outline

---
+++ src/test/java/com/wiki/steps/StepDefinitions.java
@@ -0,0 +1,11 @@
+package com.wiki.steps;
+
+import cucumber.api.java.en.Given;
+
+public class StepDefinitions {
+
+    @Given("^a search string is entered with English as selected language$")
+    public void enterSearchString() {
+
+    }
+}
---
+++ src/test/resources/com.wiki/features/search_tests.feature
@@ -0,0 +1,6 @@
+Feature: Wikipedia Search
+
+Scenario: English Search
+  Given a search string is entered with English as selected language
+#  When the search button is clicked
+#  Then the results page title matches the search string
