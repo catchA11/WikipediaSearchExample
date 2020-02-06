cd13b590fa55bf9af4487f3f530134e8a098f330
Lucy Sheehy <lucysheehy@yahoo.com>
2020-02-06 19:04:26 +0000

    Add code to open web page and log4j logging

--- src/test/java/com/wiki/steps/StepDefinitions.java
+++ src/test/java/com/wiki/steps/StepDefinitions.java
@@ -1,8 +1,21 @@
 package com.wiki.steps;
 
+import com.wiki.BrowserDriver;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.WebDriver;
 
 public class StepDefinitions {
+    BrowserDriver browserDriver = new BrowserDriver();
+    public static WebDriver driver;
+
+    @Before
+    public void openWikiHomePage() {
+        String PATH = "http://www.wikipedia.org";
+
+        driver = browserDriver.getBrowserDriver();
+        driver.get(PATH);
+    }
 
     @Given("^a search string is entered with English as selected language$")
     public void enterSearchString() {
